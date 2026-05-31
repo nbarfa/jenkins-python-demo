41062d1acafb2464c0ed93c268a577eb745522bd
Nitin Barfa <nitinbarfa260@gmail.com>
2026-05-31 17:12:55 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+name = "Nitin"
+age = 20
+
+print("Hello, my name is", name)
+print("I am learning Jenkins Pipeline!")
+print("2 + 3 =", 2 + 3)
+
+numbers = [1, 2, 3, 4, 5]
+print("Sum of numbers:", sum(numbers))
+
